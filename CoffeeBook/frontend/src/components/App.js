2a9e12e372b6ea70220d3638aa1ab185adb86752
Vincent Fouque <vnfouque@gmail.com>
2022-02-10 17:43:53 +0100

    mise en place des components

--- CoffeeBook/frontend/src/components/App.js
+++ CoffeeBook/frontend/src/components/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from "react";
+import { SearchCategory } from "./category/SearchCategory";
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    render() {
-        return <div>Hello World!</div>;
-    }
+  render() {
+    return (
+      <div className="containers d-flex">
+        <div className="row align-items-start">
+          <div className="col">
+            <div>Category</div>
+            <div>{SearchCategory}</div>
+          </div>
+          <div className="col">
+            <div>Main</div>
+          </div>
+          <div className="col">
+            <div>Contact</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ CoffeeBook/frontend/src/components/contact/Contact.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react'
+import styles from './Contact.module.css'
+
+export default function Contact() {
+    const contactList = [
+        { id: 1, firstName: "David", lastName: "Renard", imgProfile: "./img/fille" },
+        { id: 2, firstName: "laurent", lastName: "sdcs", imgProfile: "./img/fille" },
+        { id: 3, firstName: "dsijfod", lastName: "sdfsd", imgProfile: "./img/fille" },
+    ];
+    const contact = contactList.map(
+        elem => {
+            return (
+                <div key={elem.id}>
+                    <img src={elem.imgProfile} alt="" />
+                    <span>{elem.firstName} {elem.lastName}</span>
+                    <img src="icons/bootstrap-fill.svg" width="25" height="25" alt="Bootstrap"></img>
+                </div>
+            )
+        }
+    )
+
+    return (
+        <div className={styles.containers}>
+            <div >
+                <h2>Contacts</h2>
+                <div>{contact}</div>
+            </div>
+        </div>
+    )
+}
