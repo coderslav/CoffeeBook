7f76df868225969650a1c2ee98c6efcc2c96c749
rkomin <kominslav@gmail.com>
2022-02-15 16:41:48 +0100

    ADDED: Login route of already logged User

--- CoffeeBook/backend/middlewares/requireAnonym.js
+++ CoffeeBook/backend/middlewares/requireAnonym.js
@@ -1,8 +1,13 @@
+const { User } = require('../models');
+
 async function requireAnon(req, res, next) {
     if (!req.session.user) {
         next();
     } else {
-        res.status(401).send('You are already login');
+        console.log('Nice! We are here!');
+        //If user logged and access_token is valid, we will return useData object to Frontend
+        const { id, firstName, lastName, isAdmin, profilePicturePath } = await User.findOne({ where: { id: req.session.user.id }, raw: true });
+        res.status(200).send({ user: { id, isAdmin, firstName, lastName, profilePicturePath } });
     }
 }
 
