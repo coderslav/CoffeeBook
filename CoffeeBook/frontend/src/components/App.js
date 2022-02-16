9cd7a96802d980be95c7800f2c2f16fe9203dafb
Vincent Fouque <vnfouque@gmail.com>
2022-02-16 16:04:10 +0100

    maj contact

--- CoffeeBook/frontend/src/components/App.js
+++ CoffeeBook/frontend/src/components/App.js
@@ -34,58 +34,77 @@ class App extends React.Component {
       myCat: 0,
       myContact: 0,
       titleKeyword: "",
-      feedMessage: ""
+      feedMessage: "",
     };
     this.getLatest = this.getLatest.bind(this);
   }
 
   // METHODS passed as props to posts
 
-  // User has logged in 
-  // Save the user data in localStorage to keep the user signed in 
-  // until he/she signed out 
-  userHasLoggedIn = ({ id, isAdmin, firstName, lastName, profilePicturePath }) => {
+  // User has logged in
+  // Save the user data in localStorage to keep the user signed in
+  // until he/she signed out
+  userHasLoggedIn = ({
+    id,
+    isAdmin,
+    firstName,
+    lastName,
+    profilePicturePath,
+  }) => {
     localStorage.setItem("id", id);
     localStorage.setItem("isAdmin", isAdmin);
     localStorage.setItem("firstName", firstName);
     localStorage.setItem("lastName", lastName);
-    localStorage.setItem("profilePicturePath", profilePicturePath)
+    localStorage.setItem("profilePicturePath", profilePicturePath);
     this.setState({ id, isAdmin, firstName, lastName, profilePicturePath });
-  }
+  };
 
   createNewUser = () => {
     this.setState({ newAccount: true });
-  }
+  };
 
-  newUserCreated = ({ id, firstName, lastName, isAdmin, profilePicturePath }) => {
+  newUserCreated = ({
+    id,
+    firstName,
+    lastName,
+    isAdmin,
+    profilePicturePath,
+  }) => {
     localStorage.setItem("id", id);
     localStorage.setItem("isAdmin", isAdmin);
     localStorage.setItem("firstName", firstName);
     localStorage.setItem("lastName", lastName);
-    localStorage.setItem("profilePicturePath", profilePicturePath)
-    this.setState({ id, firstName, lastName, isAdmin, profilePicturePath, newAccount: false });
-  }
+    localStorage.setItem("profilePicturePath", profilePicturePath);
+    this.setState({
+      id,
+      firstName,
+      lastName,
+      isAdmin,
+      profilePicturePath,
+      newAccount: false,
+    });
+  };
 
   // Get the latest posts created in CoffeeBook
   // - "items" : posts by descending order of their creation date
   getLatest = async () => {
     try {
-        const latestReq = `http://localhost:${PORT}/latestposts`;
-        const newPosts = await axios.post(latestReq);
-        console.log("latest posts : ", newPosts);
-        this.setState({
-            posts: newPosts.data,
-            news: true,
-            best: false,
-            myCat: 0,
-            myContact: 0,
-            titleKeyword: "",
-            feedMessage: "Les dernières actualités"
-        })   
-    } catch(err) {
-        console.error("Error getting latest posts : ", err);
+      const latestReq = `http://localhost:${PORT}/latestposts`;
+      const newPosts = await axios.post(latestReq);
+      console.log("latest posts : ", newPosts);
+      this.setState({
+        posts: newPosts.data,
+        news: true,
+        best: false,
+        myCat: 0,
+        myContact: 0,
+        titleKeyword: "",
+        feedMessage: "Les dernières actualités",
+      });
+    } catch (err) {
+      console.error("Error getting latest posts : ", err);
     }
-  } 
+  };
 
   // Get best average vote posts.
   // - "items" : posts by descending order of their average vote
@@ -94,27 +113,30 @@ class App extends React.Component {
       const bestReq = `http://localhost:${PORT}/getbestposts`;
       const bestPosts = await axios.post(bestReq);
       this.setState({
-          posts: bestPosts.data,
-          news: false,
-          best: true,
-          myCatId: 0,
-          myContactId: 0,
-          titleKeyword: "",
-          feedMessage: "Les posts les mieux votés" 
+        posts: bestPosts.data,
+        news: false,
+        best: true,
+        myCatId: 0,
+        myContactId: 0,
+        titleKeyword: "",
+        feedMessage: "Les posts les mieux votés",
       });
-    } catch(err) {
+    } catch (err) {
       console.error("Error getting best posts by votes: ", err);
     }
-  }
+  };
 
-  // Get posts by category. 
-  // The request should return : 
+  // Get posts by category.
+  // The request should return :
   // - "categoryName" : The name of the category
-  // - "items" : the posts in that category by descending order of their 
+  // - "items" : the posts in that category by descending order of their
   //   creation date
   getCategoryPosts = async (e) => {
     const categoryId = e.target.value;
-    const catPosts = await axios.post(`http://localhost:${PORT}/getcategoryposts`, { categoryId });
+    const catPosts = await axios.post(
+      `http://localhost:${PORT}/getcategoryposts`,
+      { categoryId }
+    );
     this.setState({
       posts: catPosts.data,
       news: false,
@@ -122,35 +144,41 @@ class App extends React.Component {
       myCatId: categoryId,
       myContactId: 0,
       titleKeyword: "",
-      feedMessage: `Les derniers post de la catégorie ${catPosts.name}`
-    })
-  }
+      feedMessage: `Les derniers post de la catégorie ${catPosts.name}`,
+    });
+  };
 
   // Get posts from contacts
   // The request should return :
   // - "firstName","lastName" : the firstname and lastname of the contact
-  // - "items" : the posts authored by the contact by descending order 
+  // - "items" : the posts authored by the contact by descending order
   //   of their creation date
   getContactPosts = async (e) => {
     const contactId = e.target.value;
-    const contactPosts = await axios.post(`http://localhost:${PORT}/getcontactposts`, { contactId });
+    const contactPosts = await axios.post(
+      `http://localhost:${PORT}/getcontactposts`,
+      { contactId }
+    );
     this.setState({
       posts: contactPosts.data,
       news: false,
       best: false,
       myCatId: 0,
       myContactId: contactId,
-      titleKeyword: "", 
-      feedMassage: `Les derniers posts de ${contactPosts.firstName} ${contactPosts.lastName}`
-    })
-  }
+      titleKeyword: "",
+      feedMessage: `Les derniers posts de ${contactPosts.firstName} ${contactPosts.lastName}`,
+    });
+  };
 
   // Get posts which title contain a keyword
   // The request should return :
-  // - 
+  // -
   getPostsWithKeyword = async (e) => {
     const keyword = e.target.value;
-    const keywordPosts = await axios.post(`http://localhost:${PORT}/getpostswithkeyword`, { keyword });
+    const keywordPosts = await axios.post(
+      `http://localhost:${PORT}/getpostswithkeyword`,
+      { keyword }
+    );
     this.setState({
       posts: keywordPosts.data,
       news: false,
@@ -158,19 +186,19 @@ class App extends React.Component {
       myCatId: 0,
       myContactId: 0,
       titleKeyword: keyword,
-      feedMessage: `Les derniers posts avec ${keyword} en titre`
-    })
-  }
+      feedMessage: `Les derniers posts avec ${keyword} en titre`,
+    });
+  };
 
   // Display the createPost component
   createNewPost = () => {
-    this.setState({ createNewPost: true })
-  }
+    this.setState({ createNewPost: true });
+  };
 
   // Save a new post.
-  // The server should return the latest posts in CoffeeBook 
+  // The server should return the latest posts in CoffeeBook
   // by descending order of their creation date. It will possibly return the post
-  // that has been sent. 
+  // that has been sent.
   saveNewPost = async (e) => {
     e.preventDefault();
     const newPostForm = new FormData(e.target);
@@ -178,7 +206,9 @@ class App extends React.Component {
     for (let [key, value] of newPostForm) {
       newPost[key] = value;
     }
-    const newPosts = await axios.post(`http://localhost:${PORT}/post/create`, { newPost });
+    const newPosts = await axios.post(`http://localhost:${PORT}/post/create`, {
+      newPost,
+    });
     this.setState({
       createNewPost: false,
       posts: newPosts.data,
@@ -187,15 +217,18 @@ class App extends React.Component {
       myCat: 0,
       myContact: 0,
       titleKeyword: "",
-      feedMessage: "Les dernières actualités"
-    })  
-  }
+      feedMessage: "Les dernières actualités",
+    });
+  };
 
   // Route to delete a post by its id
   // return all posts by descending order of their creation date
   deletePost = async (e) => {
     console.log(`delete post ${e.currentTarget.id}`);
-    const updatedPostList = await axios.post(`http://localhost:${PORT}/post/delete`, { id: e.currentTarget.id });
+    const updatedPostList = await axios.post(
+      `http://localhost:${PORT}/post/delete`,
+      { id: e.currentTarget.id }
+    );
     let refreshedList = updatedPostList.data.slice();
     if (this.state.best) {
       refreshedList.sort((a, b) => a.voteAvg - b.voteAvg);
@@ -207,29 +240,33 @@ class App extends React.Component {
     }
 
     if (this.state.myContact) {
-      refreshedList = refreshedList.filter(post => post.userId === this.state.myContact );
+      refreshedList = refreshedList.filter(
+        (post) => post.userId === this.state.myContact
+      );
     }
 
     if (this.state.keyword) {
-      refreshedList = refreshedList.filter(post => post.title.contains(this.state.keyword));
+      refreshedList = refreshedList.filter((post) =>
+        post.title.contains(this.state.keyword)
+      );
     }
     this.setState({
       posts: refreshedList,
-    })
-  }
+    });
+  };
 
-  // In case a previous user signed in to CoffeeBook, his/her info are retrieved from 
-  // archive in localStorage. 
+  // In case a previous user signed in to CoffeeBook, his/her info are retrieved from
+  // archive in localStorage.
   // As there is an user "id" when this component mounts, the Login screen will be called
   componentDidMount() {
     if (localStorage.getItem("id")) {
       this.setState({
         id: localStorage.getItem("id"),
         firstName: localStorage.getItem("firstName"),
-        lastName: localStorage.getItem('lastName'),
+        lastName: localStorage.getItem("lastName"),
         isAdmin: localStorage.getItem("isAdmin"),
-        profilePicturePath: localStorage.getItem("profilePicturePath")
-      })
+        profilePicturePath: localStorage.getItem("profilePicturePath"),
+      });
     }
   }
 
@@ -238,56 +275,64 @@ class App extends React.Component {
       // axios.post(`http://localhost:${PORT}/login`)
       // .then(this.getLatest());
       this.getLatest();
-    } 
+    }
   }
 
   render() {
-    return (
-      this.state.id == 0 && !this.state.newAccount
-        ? <Login loggedUser={this.userHasLoggedIn} createUser={this.createNewUser}/>
-        : this.state.newAccount 
-        ? <Subscribe newUserCreated={this.newUserCreated} />
-        :  <div className="container">
-            <Route exact path="/">
-              <div className="row ">
-                <div className="col-3 category maxHeight">
-                  <LogoCB />
-                  <CategoryFilter getLatest={this.getLatest} getBest={this.getBest} />
-                  <MyCategories getCategoryPosts={this.getCategoryPosts} />
-                </div>
-                <div className="col-6 profilSection">
-                  <div className="headerProfil">
-                    <HeaderProfile 
-                      userId={this.state.id} 
-                      isAdmin={this.state.isAdmin} 
-                      firstName={this.state.firstName} 
-                      lastName={this.state.lastName} 
-                      profilePicturePath={this.profilePicturePath} />
-                  </div>
-                  <div className="sectionPost">
-                    <Post 
-                      getPostsWithKeyword={this.getPostsWithKeyword} 
-                      createNewPost={this.createNewPost} />
-                    {
-                     this.state.createNewPost 
-                      ? <CreatePost saveNewPost={this.saveNewPost}/>
-                      : ""
-                    }  
-                  </div>
-                  <div>
-                    <Actualites feedMessage={this.state.feedMessage} posts={this.state.posts}/>
-                  </div>
-                </div>
-                <div className="col-3 contact d-flex flex-row justify-content-center align-items-start ">
-                  <Contact getContactPosts={this.getContactPosts}/>
-                </div>
+    return this.state.id == 0 && !this.state.newAccount ? (
+      <Login
+        loggedUser={this.userHasLoggedIn}
+        createUser={this.createNewUser}
+      />
+    ) : this.state.newAccount ? (
+      <Subscribe newUserCreated={this.newUserCreated} />
+    ) : (
+      <div className="container">
+        <Route exact path="/">
+          <div className="row ">
+            <div className="col-3 category maxHeight">
+              <LogoCB />
+              <CategoryFilter
+                getLatest={this.getLatest}
+                getBest={this.getBest}
+              />
+              <MyCategories getCategoryPosts={this.getCategoryPosts} />
+            </div>
+            <div className="col-6 profilSection">
+              <div className="headerProfil">
+                <HeaderProfile
+                  userId={this.state.id}
+                  isAdmin={this.state.isAdmin}
+                  firstName={this.state.firstName}
+                  lastName={this.state.lastName}
+                  profilePicturePath={this.profilePicturePath}
+                />
               </div>
-            </Route>
+              <div className="sectionPost">
+                <Post
+                  getPostsWithKeyword={this.getPostsWithKeyword}
+                  createNewPost={this.createNewPost}
+                />
+                {this.state.createNewPost ? (
+                  <CreatePost saveNewPost={this.saveNewPost} />
+                ) : (
+                  ""
+                )}
+              </div>
+              <div>
+                <Actualites
+                  feedMessage={this.state.feedMessage}
+                  posts={this.state.posts}
+                />
+              </div>
+            </div>
+            <div className="col-3 contact d-flex flex-row justify-content-center align-items-start ">
+              <Contact getContactPosts={this.getContactPosts} />
+            </div>
           </div>
-      
-
-
-      );
+        </Route>
+      </div>
+    );
   }
 }
 export default App;
--- CoffeeBook/frontend/src/components/contact/Contact.jsx
+++ CoffeeBook/frontend/src/components/contact/Contact.jsx
@@ -3,39 +3,46 @@ import SearchCategory from '../category/SearchCategory';
 import './contact.css'
 import { AiOutlineMinusCircle } from "react-icons/ai";
 
-export default function Contact() {
-    const contactList = [
-        { id: 1, firstName: "David", lastName: "Renard", imgProfile: "/images/woman.jpg" },
-        { id: 2, firstName: "laurent", lastName: "sdcs", imgProfile: "./images/woman2.jpg" },
-        { id: 3, firstName: "dsijfod", lastName: "sdfsd", imgProfile: "./images/man.jpg" },
-    ];
-
-    const contact = contactList.map(
-        elem => {
-            return (
-                <div className='blocContacts d-flex justify-content-start align-items-center' key={elem.id}>
-                    <img src={elem.imgProfile} alt="img_profil" />
-                    <p>{elem.firstName} {elem.lastName}</p>
-                    <div>
-                        <AiOutlineMinusCircle/>
-                    </div>
-                </div>
-            )
+export default class Contact extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            firstName: "vincent",
+            lastName: "Fouque"
         }
-    )
-    return (
 
-        <div className='d-flex flex-column'>
-            <span className='titreContact'>Contacts</span>
-            <div>{contact}</div>
-            <div className=''>
-                <button className='btnSeeMore'>Voir plus</button>
+        // const contactList = [
+        //     { id: 1, firstName: "David", lastName: "Renard", imgProfile: "/images/woman.jpg" },
+        //     { id: 2, firstName: "laurent", lastName: "sdcs", imgProfile: "./images/woman2.jpg" },
+        //     { id: 3, firstName: "dsijfod", lastName: "sdfsd", imgProfile: "./images/man.jpg" },
+        // ];
+        const contact = this.state.map(
+            elem => {
+                return (
+                    <div className='blocContacts d-flex justify-content-start align-items-center' key={elem.id}>
+                        <img src={elem.imgProfile} alt="img_profil" />
+                        <p value={elem.id} onClick={this.props.getContactPosts}>{elem.this.state.firstName} {elem.this.state.lastName}</p>
+                        <div>
+                            <AiOutlineMinusCircle />
+                        </div>
+                    </div>
+                )
+            }
+        )
+    }
+    render() {
+        return (
+            <div className='d-flex flex-column' >
+                <span className='titreContact'>Contacts</span>
+                <div>{contact}</div>
+                <div className=''>
+                    <button className='btnSeeMore'>Voir plus</button>
+                </div>
+                <div className="editList">
+                    <button className='bntEdit'>Editer la liste</button>
+                </div>
+                <SearchCategory title={"Chercher un contact"} placeholder={"contact"} />
             </div>
-            <div className="editList">
-                <button className='bntEdit'>Editer la liste</button>
-            </div>
-            <SearchCategory title={"Chercher un contact"} placeholder={"contact"} />
-        </div>
-
-    )
+        )
+    }
 }
