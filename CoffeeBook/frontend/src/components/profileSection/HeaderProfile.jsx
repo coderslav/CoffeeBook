b92e1870a200dbcf6b6e2de8480f94a3b2c65c0f
Vincent Fouque <vnfouque@gmail.com>
2022-02-16 10:45:51 +0100

    creation component modifyDatatProfile

--- CoffeeBook/frontend/src/components/profileSection/HeaderProfile.jsx
+++ CoffeeBook/frontend/src/components/profileSection/HeaderProfile.jsx
@@ -14,15 +14,23 @@ export class HeaderProfile extends Component {
                 <img className="imgProfile" src={man} alt="" width="70px" height="70px" />
                 <span>Prénom</span>
                 <span>Nom</span>
-                <div className='parameter'>                   
+                <div className='parameter'>
                     <div className="nav-item dropdown">
-                        <a className="nav-link gearAppearance" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"> <BsFillGearFill /></a>
+                        <a className="nav-link gearAppearance" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><BsFillGearFill /></a>
                         <ul className="dropdown-menu menuParameter">
-                            <li className='d-flex justify-content-start align-items-center ml-2'><a className="dropdown-item" href="#"><BsFillPencilFill/>Modifier les données</a></li>
-                            <li className='d-flex justify-content-start align-items-center'><a className="dropdown-item" href="#"><VscDebugDisconnect/>Se déconnecter</a></li>
-                            <li className='d-flex justify-content-start align-items-center'><a className="dropdown-item" href="#"><MdDeleteForever/>Supprimer son compte</a></li>
+                            <li className='d-flex justify-content-start align-items-center ml-2'>
+                                <a className="dropdown-item" href="#">
+                                    <div className="nav-item dropdown">
+                                        <a className="nav-link gearAppearance" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                                            <BsFillPencilFill />Modifier les données
+                                        </a>
+                                    </div>
+                                </a>
+                            </li>
+                            <li className='d-flex justify-content-start align-items-center'><a className="dropdown-item" href="#"><VscDebugDisconnect />Se déconnecter</a></li>
+                            <li className='d-flex justify-content-start align-items-center'><a className="dropdown-item" href="#"><MdDeleteForever />Supprimer son compte</a></li>
                         </ul>
-                    </div>                   
+                    </div>
                 </div>
             </div>
         )
