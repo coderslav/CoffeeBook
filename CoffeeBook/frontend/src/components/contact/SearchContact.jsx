6a1bcc511d35d1619edb4b6d466da8c6e89d4978
tlucas <tristan.lucas@gmail.com>
2022-02-17 10:06:13 +0100

    Search contacts

---
+++ CoffeeBook/frontend/src/components/contact/SearchContact.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import "../category/category.css";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+
+const PORT = 5000;
+
+export default class SearchContact extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchResults: []
+    }
+    this.searchTimeout = null;
+    this.searchContacts = this.searchContacts.bind(this);
+  }
+
+  searchContacts = (e) => {
+    if (this.searchTimeout) {
+      clearTimeout(this.searchTimeout);
+    }
+
+    const contactKeyword = e.target.value;
+    // const userId = this.props.userId;
+
+    this.searchTimeout = setTimeout(() => {
+      axios.post(`http://localhost:${PORT}/user/filter`, {
+        filter: contactKeyword,
+        userId: this.props.userId
+      })
+        .then(res => {
+          console.log("results from contact search by keyword : ", res);
+          if (res.data.length) {
+            this.setState({ searchResults: res.data })
+          } else {
+            this.setState({ searchResults: [] })
+          }
+        })
+        .catch(err => console.log("Error while searching contacts by keyword", err))
+    }, 600);
+
+
+  }
+
+  render() {
+    return (
+      <div className='container-fluid searchCategory'>
+        <span>{this.props.title}</span>
+        <input 
+          className='container-fluid' type="text" placeholder={this.props.placeholder} 
+          onChange={this.searchContacts}/>
+        {
+          this.state.searchResults.length
+            ?
+            this.state.searchResults.map(contact => {
+              return (
+                <div className='d-flex justify-content-center align-items-start ms-5' key={contact.id}>
+                  <p>#{contact.firstName} {contact.lastName}</p>
+                  <div className='ms-3'>
+                    <AiOutlinePlusCircle />
+                  </div>
+                </div>
+              )
+            })
+            : ""
+        }
+      </div>
+    )
+  }
+}
