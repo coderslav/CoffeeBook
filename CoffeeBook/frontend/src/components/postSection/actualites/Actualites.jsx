cc679e330162cad61964013d2e74435688521a12
rkomin <kominslav@gmail.com>
2022-02-17 17:56:36 +0100

    ADDED: MUI Icons, Redirection to login when Token expire, Post offset loading; FIX: Rating Stars color - white

--- CoffeeBook/frontend/src/components/postSection/actualites/Actualites.jsx
+++ CoffeeBook/frontend/src/components/postSection/actualites/Actualites.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import PostActu from '../postActu/PostActu.jsx'
-import './actualites.css'
+import React from 'react';
+import PostActu from '../postActu/PostActu.jsx';
+import './actualites.css';
+import { IconButton } from '@mui/material';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
-
-export default function Actualites({feedMessage, posts}) {
-   return (
-      <div className='container'>
-         <p className='actuTitle'>{feedMessage}</p>
-         { posts.map(p => <PostActu key={p.id} post={p}/>) }
-      </div>
-   )
+export default function Actualites({ feedMessage, posts, addActualites }) {
+    return (
+        <div className='container'>
+            <p className='actuTitle'>{feedMessage}</p>
+            {posts.map((p) => (
+                <PostActu key={p.id} post={p} />
+            ))}
+            <IconButton aria-label='add to shopping cart' onClick={addActualites} sx={{ color: 'orange' }}>
+                <AddBoxIcon fontSize='large' />
+            </IconButton>
+        </div>
+    );
 }
