7c5fd78818a3700fcdfdf5a2618bf2506a275ce7
rkomin <kominslav@gmail.com>
2022-02-17 14:23:28 +0100

    ADDED: Route for getting all posts by - order of creation and order of average note with offset=request and limit=10

--- CoffeeBook/backend/routes/index.js
+++ CoffeeBook/backend/routes/index.js
@@ -8,12 +8,30 @@ router.post('/', requireAuthenticate, (req, res) => {
     console.log(req.session.user);
 });
 
+// Get all posts by order of creation with offset=request and limit=10
 router.post('/latestposts', requireAuthenticate, async (req, res) => {
     const posts = JSON.parse(
         JSON.stringify(
             await Post.findAll({
                 order: [['createdAt', 'DESC']],
                 include: ['postCategory', 'postUser'],
+                offset: req.body.offset ? req.body.offset : 0,
+                limit: 10,
+            })
+        )
+    );
+    res.send(posts);
+});
+
+//Get all posts by order of average note with offset=request and limit=10
+router.post('/bestposts', requireAuthenticate, async (req, res) => {
+    const posts = JSON.parse(
+        JSON.stringify(
+            await Post.findAll({
+                order: [['voteAvg', 'DESC']],
+                include: ['postCategory', 'postUser'],
+                offset: req.body.offset ? req.body.offset : 0,
+                limit: 10,
             })
         )
     );
