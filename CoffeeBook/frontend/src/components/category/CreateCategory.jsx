1780758bec018f535a63c32e710575040ecd5e8e
tlucas <tristan.lucas@gmail.com>
2022-02-18 07:50:20 +0100

    add submit button

--- CoffeeBook/frontend/src/components/category/CreateCategory.jsx
+++ CoffeeBook/frontend/src/components/category/CreateCategory.jsx
@@ -6,12 +6,15 @@ export default class CreateCategory extends Component {
 
   render() {
     return (
-      <>
+      <div className='d-flex flex-column'>
         <p className="h4">Créer une catégorie</p>
         <form action="" method="post">
           <input type="text" name="newCategory" placeholder="ex: Paris 2024" />
+          <div className='mt-2 d-flex justify-content-center'>
+            <input type="submit" value="Créer" className='ml-auto'/>
+          </div>
         </form>
-      </>
+      </div>
     )
   }
 }
