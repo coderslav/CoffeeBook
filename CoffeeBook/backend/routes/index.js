2b5287f279cdfb5c3d35528e8d5b3a8a5c5dc9bd
coderslav <coderslav@gmail.com>
2022-02-20 19:54:01 +0100

    BACKEND >>> ADDED: New relation between Post - PostComment, New route /postsbykeyword (2 variants) for searching Posts by keyword; CHANGED: /latestpost, /bestposts, user/filter routes; FRONTEND >>> ADDED: Search Post by keyword logic; CHANGED: addActualites handler, addActualites button (disappears when not needed)

--- CoffeeBook/backend/routes/index.js
+++ CoffeeBook/backend/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { Post, Category, User } = require('../models');
+const { Post, Category, User, PostComment } = require('../models');
 const requireAuthenticate = require('../middlewares/requireAuthenticate');
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 router.post('/', requireAuthenticate, (req, res) => {
     res.send(`Welcome to CoffeeBook ! \n`);
@@ -13,7 +15,12 @@ router.post('/latestposts', requireAuthenticate, async (req, res) => {
         JSON.stringify(
             await Post.findAll({
                 order: [['createdAt', 'DESC']],
-                include: ['postCategory', 'postUser'],
+                attributes: { exclude: ['userId'] },
+                include: [
+                    { model: Category, as: 'postCategory', through: { attributes: [] }, attributes: { exclude: ['createdAt', 'updatedAt'] } },
+                    { model: User, as: 'postUser', attributes: { exclude: ['password', 'token', 'createdAt', 'updatedAt'] } },
+                    { model: PostComment, as: 'comments', attributes: { exclude: ['userId', 'postId'] }, include: { model: User, as: 'user', attributes: { exclude: ['password', 'token', 'createdAt', 'updatedAt'] } } },
+                ],
                 offset: req.body.offset ? req.body.offset : 0,
                 limit: 10,
             })
@@ -28,7 +35,12 @@ router.post('/bestposts', requireAuthenticate, async (req, res) => {
         JSON.stringify(
             await Post.findAll({
                 order: [['voteAvg', 'DESC']],
-                include: ['postCategory', 'postUser'],
+                attributes: { exclude: ['userId'] },
+                include: [
+                    { model: Category, as: 'postCategory', through: { attributes: [] }, attributes: { exclude: ['createdAt', 'updatedAt'] } },
+                    { model: User, as: 'postUser', attributes: { exclude: ['password', 'token', 'createdAt', 'updatedAt'] } },
+                    { model: PostComment, as: 'comments', attributes: { exclude: ['userId', 'postId'] }, include: { model: User, as: 'user', attributes: { exclude: ['password', 'token', 'createdAt', 'updatedAt'] } } },
+                ],
                 offset: req.body.offset ? req.body.offset : 0,
                 limit: 10,
             })
@@ -41,13 +53,13 @@ router.post('/getuserposts', requireAuthenticate, async (req, res) => {
     const posts = JSON.parse(
         JSON.stringify(
             await User.findOne({
-                where : { id : req.body.userId },
-                include: { 
-                    model: Post, 
-                    as: "userPost", 
+                where: { id: req.body.userId },
+                include: {
+                    model: Post,
+                    as: 'userPost',
                     order: [['createdAt', 'DESC']],
                     include: ['postCategory', 'postUser'],
-                }
+                },
             })
         )
     );
@@ -59,13 +71,74 @@ router.post('/getcategoryposts', requireAuthenticate, async (req, res) => {
         JSON.stringify(
             await Category.findOne({
                 where: {
-                    id: req.body.categoryId 
+                    id: req.body.categoryId,
                 },
-                include: { model: Post, as: 'categoryPost', include: ['postCategory', 'postUser'] }
+                include: { model: Post, as: 'categoryPost', include: ['postCategory', 'postUser'] },
             })
         )
-    ); 
+    );
     res.send(posts);
-})
+});
+
+// Get posts by keyword with/without offset (JS filter by postTitle and categoryName)
+router.post('/postsbykeyword', requireAuthenticate, async (req, res) => {
+    if (req.body.filter) {
+        const filterCondition = req.body.filter.toString().toLowerCase().replaceAll(' ', '');
+        try {
+            const posts = JSON.parse(
+                JSON.stringify(
+                    await Post.findAll({
+                        order: [['createdAt', 'DESC']],
+                        attributes: { exclude: ['userId'] },
+                        include: [
+                            { model: Category, as: 'postCategory', through: { attributes: [] }, attributes: { exclude: ['createdAt', 'updatedAt'] } },
+                            { model: User, as: 'postUser', attributes: { exclude: ['password', 'token', 'createdAt', 'updatedAt'] } },
+                            { model: PostComment, as: 'comments', attributes: { exclude: ['userId', 'postId'] }, include: { model: User, as: 'user', attributes: { exclude: ['password', 'token', 'createdAt', 'updatedAt'] } } },
+                        ],
+                    })
+                )
+            );
+            let filteredPosts = posts.filter((post) => {
+                let postTitleCatNameConcat = (post.title + (post.postCategory.length > 0 ? post.postCategory.map((postCat) => postCat.name).join('') : '')).toLowerCase().replaceAll(' ', '');
+                return postTitleCatNameConcat.includes(filterCondition);
+            });
+            res.send(filteredPosts.slice(req.body.offset ? req.body.offset : 0, req.body.offset ? req.body.offset + 10 : 10));
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    } else {
+        res.status(400).send('Filter string is not provided');
+    }
+});
+
+// Get posts by keyword with/without offset (Sequelize filter by postTitle only)
+// router.post('/postsbykeyword', requireAuthenticate, async (req, res) => {
+//     if (req.body.filter) {
+//         const filterCondition = req.body.filter.toString()
+//         try {
+//             const posts = JSON.parse(
+//                 JSON.stringify(
+//                     await Post.findAll({
+//                         where: { title: { [Op.iLike]: `%${filterCondition}%` } },
+//                         order: [['createdAt', 'DESC']],
+//                         attributes: { exclude: ['userId'] },
+//                         include: [
+//                             { model: Category, as: 'postCategory', through: { attributes: [] }, attributes: { exclude: ['createdAt', 'updatedAt'] } },
+//                             { model: User, as: 'postUser', attributes: { exclude: ['password', 'token', 'createdAt', 'updatedAt'] } },
+//                             { model: PostComment, as: 'comments', attributes: { exclude: ['userId', 'postId'] }, include: { model: User, as: 'user', attributes: { exclude: ['password', 'token', 'createdAt', 'updatedAt'] } } },
+//                         ],
+//                         offset: req.body.offset ? req.body.offset : 0,
+//                         limit: 10,
+//                     })
+//                 )
+//             );
+//             res.send(posts);
+//         } catch (error) {
+//             res.status(500).send(error);
+//         }
+//     } else {
+//         res.status(400).send('Filter string is not provided');
+//     }
+// });
 
 module.exports = router;
